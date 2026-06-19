8871cce315ee9bf1fba88c1a467fcc063682dff1
TristanStarkl <thomas.rety57@gmail.com>
2026-06-19 13:42:22 +0200

    chore: remove Python statement parser

--- src/statements/close_statement.py
+++
@@ -1,9 +0,0 @@
-from .base_statement import Statement
-
-class CloseStatement(Statement):
-    def __init__(self, type="close", statement=None):
-        super().__init__(type, statement)
-        self.file = statement.split(' ')[1]
-
-    def display(self) -> str:
-        return f"CLOSED FILE: {self.file}"
