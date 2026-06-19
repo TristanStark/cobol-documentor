5cdc3497c8169c64a51a3466b0476ae438553190
TristanStarkl <thomas.rety57@gmail.com>
2026-06-19 13:44:36 +0200

    chore: remove Python statement parser

--- src/statements/line_statement.py
+++
@@ -1,6 +0,0 @@
-from .base_statement import Statement
-
-class LineStatement(Statement):
-    def __init__(self, statement: str):
-        super().__init__(statement=statement)
-        self.typeStatement = "line"
